187d24e40ae71994fb7575525830e86c6ff2a87f
Calvin-braus <75593027+calvnvn@users.noreply.github.com>
2025-12-29 22:26:56 +0700

    Initial Laravel setup

---
+++ database/migrations/2025_12_29_151040_create_farmers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('farmers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik', 16)->unique();
+            $table->string('full_name');
+
+            // Land precision agriculture details
+            $table->decimal('land_area_ha', 5, 2);
+
+            // calculation land area x standard yield per hectare
+            $table->decimal('yield_capacity_limit', 8, 2)->nullable();
+
+            // Farmer grading system
+            $table->char('current_grade', 1)->default('B');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('farmers');
+    }
+};
